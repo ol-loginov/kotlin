b1fb8586b769512336602e1147407d166c99e90f
Andrey Breslav <andrey.breslav@jetbrains.com>
2013-05-16 19:57:09 +0400

    Serialization tests reuse test data from loadKotlin

--- generators/org/jetbrains/jet/generators/tests/GenerateTests.java
+++ generators/org/jetbrains/jet/generators/tests/GenerateTests.java
@@ -31,6 +31,7 @@ import org.jetbrains.jet.completion.AbstractJavaCompletionTest;
 import org.jetbrains.jet.completion.AbstractJavaWithLibCompletionTest;
 import org.jetbrains.jet.completion.AbstractJetJSCompletionTest;
 import org.jetbrains.jet.completion.AbstractKeywordCompletionTest;
+import org.jetbrains.jet.descriptors.serialization.AbstractDescriptorSerializationTest;
 import org.jetbrains.jet.jvm.compiler.*;
 import org.jetbrains.jet.lang.psi.AbstractJetPsiMatcherTest;
 import org.jetbrains.jet.lang.resolve.lazy.AbstractLazyResolveDescriptorRendererTest;
@@ -210,6 +211,13 @@ public class GenerateTests {
                 "ModuleXmlParserTestGenerated",
                 AbstractModuleXmlParserTest.class,
                 testModel("compiler/testData/modules.xml", true, "xml", "doTest")
+        );
+
+        generateTest(
+                "compiler/tests/",
+                "DescriptorSerializationTestGenerated",
+                AbstractDescriptorSerializationTest.class,
+                testModel("compiler/testData/loadKotlin")
         );
 
         generateTest(
